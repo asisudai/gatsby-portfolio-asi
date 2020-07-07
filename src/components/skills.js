01e60474627f330bf93b2eb065b2fec158bd2fb1
NetflixBox <asi@netflixbox.nelson>
2020-07-06 18:39:58 -0700

    one more skill

--- src/components/skills.js
+++ src/components/skills.js
@@ -11,75 +11,73 @@ const Container = styled.div`
     width: 50%;
 `
 
+// TODO: move this to a data module.
+const skillslist = [
+  { 'name': 'Python', 'link': 'http://www.python.org' },
+  { 'name': 'Shell', 'link': 'https://ohmyz.sh/' },
+  { 'name': 'MySQL', 'link': 'https://www.mysql.com/' },
+  { 'name': 'MongoDB', 'link': 'https://www.mongodb.com/' },
+  { 'name': 'Docker', 'link': 'http://www.docker.com' },
+  { 'name': 'ESXi', 'link': 'https://www.vmware.com/ca/products/esxi-and-esx.html' },
+  { 'name': 'Grafana', 'link': 'https://grafana.com/' },
+  { 'name': 'Promethues', 'link': 'https://prometheus.io/' },
+  { 'name': 'Promethues', 'link': 'https://prometheus.io/' },
+  { 'name': 'Shotgun', 'link': 'https://www.shotgunsoftware.com/' },
+  { 'name': 'Rest API', },
+  { 'name': 'Javascript', },
+  { 'name': 'HTML', },
+  { 'name': 'CSS', },
+  { 'name': 'React', 'link': 'https://reactjs.org/' },
+  { 'name': 'Flask', 'link': 'https://flask.palletsprojects.com/' },
+  { 'name': 'Django', 'link': 'https://www.djangoproject.com/' },
+  { 'name': 'PyQT', 'link': 'https://doc.bccnsoft.com/docs/PyQt5/' },
+  { 'name': 'Maya', 'link': 'https://www.autodesk.com/products/maya/overview' },
+  { 'name': 'Nuke', 'link': 'https://www.foundry.com/products/nuke' },
+  { 'name': 'Redshift', 'link': 'https://www.redshift3d.com/' },
+  { 'name': 'Renderman', 'link': 'https://renderman.pixar.com/' },
+  { 'name': 'Vray', 'link': 'https://www.chaosgroup.com/' },
+  { 'name': 'Deadline', 'link': 'https://www.awsthinkbox.com/deadline' },
+  { 'name': 'Qube', 'link': 'https://www.pipelinefx.com/  ' },
+  { 'name': 'Pipeline' },
+  { 'name': 'Rigging' },
+  { 'name': 'Web-Dev' },
+  { 'name': 'Sys Admin' },
+  { 'name': 'Supervisor' },
+];
+
 
 const Skill = ({skill}) => {
 
   if (skill.link) {
       return (
         <Chip label={skill.name}
-            color="green"
             size='small'
-            color={skill.color ? skill.color : "primary"}
+            color="primary"
             component="a"
             target="_blank"
             clickable
             href={skill.link ? skill.link : "/"}
             variant = "outlined" />
       )
-    } else {
+  } else {
       return(
         <Chip label={skill.name}
-          color="green"
           size='small'
-          color={skill.color ? skill.color : "primary"}
+          color="primary"
           variant="outlined" />
       )
-    }
+  }
 }
 
 const Skills = ({data}) => {
 
-    // TODO: move this to a data module.
-    const skillslist = [
-                        { 'name': 'Python', 'link': 'http://www.python.org' },
-                        { 'name': 'Shell', 'link': 'https://ohmyz.sh/' },
-                        { 'name': 'MySQL', 'link': 'https://www.mysql.com/' },
-                        { 'name': 'MongoDB', 'link': 'https://www.mongodb.com/' },
-                        { 'name': 'Docker', 'link': 'http://www.docker.com' },
-                        { 'name': 'ESXi', 'link': 'https://www.vmware.com/ca/products/esxi-and-esx.html' },
-                        { 'name': 'Grafana', 'link': 'https://grafana.com/' },
-                        { 'name': 'Promethues', 'link': 'https://prometheus.io/' },
-                        { 'name': 'Shotgun', 'link': 'https://www.shotgunsoftware.com/' },
-                        { 'name': 'Rest API',  },
-                        { 'name': 'Javascript',  },
-                        { 'name': 'HTML', },
-                        { 'name': 'CSS', },
-                        { 'name': 'React', 'link': 'https://reactjs.org/' },
-                        { 'name': 'Flask', 'link': 'https://flask.palletsprojects.com/' },
-                        { 'name': 'Django', 'link': 'https://www.djangoproject.com/' },
-                        { 'name': 'PyQT', 'link': 'https://doc.bccnsoft.com/docs/PyQt5/' },
-                        { 'name': 'Maya', 'link': 'https://www.autodesk.com/products/maya/overview' },
-                        { 'name': 'Nuke', 'link': 'https://www.foundry.com/products/nuke' },
-                        { 'name': 'Redshift', 'link': 'https://www.redshift3d.com/' },
-                        { 'name': 'Renderman', 'link': 'https://renderman.pixar.com/' },
-                        { 'name': 'Vray', 'link': 'https://www.chaosgroup.com/' },
-                        { 'name': 'Deadline', 'link': 'https://www.awsthinkbox.com/deadline' },
-                        { 'name': 'Qube', 'link': 'https://www.pipelinefx.com/  ' },
-                        { 'name': 'Pipeline' },
-                        { 'name': 'Rigging' },
-                        { 'name': 'Web-Dev' },
-                        { 'name': 'Sys Admin' },
-                        { 'name': 'Supervisor' },
-                        ];
-
-
-    return (
+      return (
         <SContainer title="Skills" description="Things I do">
-            <Container color="text.primary">
-                {skillslist.map((skill) => (
-                  <Skill skill={skill} />
-                  ))}
-            </Container>
+          <Container color="text.primary">
+            {skillslist.map((skill) => (
+              <Skill skill={skill} />
+              ))}
+          </Container>
         </SContainer>
     )
 }
