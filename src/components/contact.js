14ff0f0d56d7329755ad3306e7f4d8c260c5272d
NetflixBox <asi@netflixbox.nelson>
2020-07-12 19:25:11 -0700

    getting contact to work

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,76 +1,83 @@
-import React from "react"
+import React from 'react';
 import SContainer from "../components/container"
+import styled from "@emotion/styled"
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 
-export default class Contact extends React.Component {
-    state = {
-        name: "",
-        email: "",
-        message: "",
+const Container = styled.div`
+  width: 75%;
+  margin: auto;
+`
+
+// export default class Contact extends React.Component {
+const Contact = () => {
+
+  const [value, setValue] = React.useState(0);
+
+  function handleInputChange(event) {
+
+    let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (event.target.name==="email") {
+      if (re.test(event.target.value)) {
+        setValue(false);
+      } else {
+        setValue(true);
+      }
     }
+  }
 
-    handleInputChange = event => {
-        const target = event.target
-        const value = target.value
-        const name = target.name
 
-        this.setState({
-            [name]: value,
-        })
-
-        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-        if (name==="email") {
-            if (re.test(value)) {
-                // valid email
-                // target.setState({ 'error': false })
-            }
-            else {
-                // invalid email, maybe show an error to the user.
-                console.log(name, value, 'no good')
-                // target.setState({'error':'true'})
-                // this.setState({'error': event.target.true})
-
-            }
-        }
-    }
-
-    render() {
-        return (
-            <SContainer title='Contact'>
-                <form method="post"
-                    action="https://formspree.io/asisudai@gmail.com"
-                    autoComplete="off">
-                    <TextField id="message"
-                        label="Message"
-                        name="message"
-                        rows={3}
-                        fullWidth
-                        multiline
-                        helpText="Any message or question?"
-                        variant="outlined"
-                        onChange={this.handleInputChange}
-                        />
-                    <TextField id="name" required="true"
-                        label="Name"
-                        name="name"
-                        helpText="How you would like to be called?"
-                        variant="outlined"
-                        onChange={this.handleInputChange}
-                        />
-                    <TextField id="email" required="true"
-                        label="Email"
-                        type="email"
-                        name="email"
-                        helpText="What is your email?"
-                        variant="outlined"
-                        onChange={this.handleInputChange}
-                        />
-                    <Button type="submit">Send It</Button>
-                </form>
-            </SContainer>
-        )
-    }
+  return (
+      <SContainer title='Contact'>
+        <Container>
+          <form method="post"
+              action="https://formspree.io/asisudai@gmail.com"
+              autoComplete="off">
+              <TextField id="name" required="true"
+                  error={ false }
+                  label="Name"
+                  name="Name"
+                  fullWidth
+                  style={{ marginTop: 8 }}
+                  helpText="How you would like to be called?"
+                  variant="outlined"
+                  onChange={handleInputChange}
+              />
+              <TextField id="email" required="true"
+                label="Email"
+                type="email"
+                name="email"
+                error={ value }
+                fullWidth
+                style={{ marginTop: 8 }}
+                helpText="What is your email?"
+                variant="outlined"
+                onChange={handleInputChange}
+              />
+              <TextField id="message"
+                label="Message"
+                name="message"
+                rows={3}
+                fullWidth
+                style={{ marginTop: 8 }}
+                multiline
+                helpText="Any message or question?"
+                variant="outlined"
+                onChange={handleInputChange}
+              />
+              <Button variant="outlined"
+                color="primary"
+                style={{ marginTop: 8 }}
+                fullWidth
+                type="submit">
+                  Send It
+              </Button>
+          </form>
+        </Container>
+      </SContainer>
+  )
 }
+
+export default Contact
